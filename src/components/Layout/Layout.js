38c512c4d1fd5c7779322ccc03c6a28ca4fda709
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-07 21:43:30 +0900

    Add navigation bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import Aux from '../../hoc/Aux'
-import classes from './Layout.module.css'
+import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>{props.children}</main>
     </Aux>
 );
 
-export default layout
+export default layout;
